147f60e04c83df4952fe61c82c92dfcab51ce893
Alan Fidelino <afidedev26@gmail.com>
2017-02-13 18:40:55 +1100

    Rename example page

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -3,7 +3,7 @@ var React          = require('react');
 // Stateless Functional Component
 var Examples = (props)=>{
   return(
-    <h2 className="text-center">Examples Component</h2>
+    <h2 className="text-center">Examples</h2>
   );
 }
 module.exports = Examples;
