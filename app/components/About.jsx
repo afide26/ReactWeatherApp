c567c961642c14af79e79e8dba499d2d664c9768
Alan Fidelino <afidedev26@gmail.com>
2017-02-16 19:49:05 +1100

    Add styling to About page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -4,8 +4,22 @@ var React          = require('react');
 var About = (props)=>{
   return(
     <div>
-      <h2 className="text-center">About</h2>
-      <p className="text-center">Welcome to the About Page</p>
+      <h1 className="text-center">About</h1>
+      <p>
+        This is a sample application from The Complete React Developer Course
+        by Andrew Mead.
+        Checkout this great course and begin studying it at
+
+      </p>
+      <h5 className="text-center">
+        <a href="https://www.udemy.com/the-complete-react-web-app-developer-course" target="_blank"><i className= "fa fa-graduation-cap "></i> Udemy</a>
+      </h5>
+      <p>The repository for this application can be found here:</p>
+      <a href="https://github.com/afide26/ReactWeatherApp" target="_blank">
+        <h5 className="text-center">
+          <i className="fa fa-github fa-fw"></i> React Weather App
+        </h5>
+      </a>
     </div>
 
   )
