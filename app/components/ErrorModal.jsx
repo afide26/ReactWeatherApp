99f966b392465b9759ea9e55a1c93ede532700f4
Alan Fidelino <afidedev26@gmail.com>
2017-02-17 21:15:22 +1100

    Fix error modal errors

--- app/components/ErrorModal.jsx
+++ app/components/ErrorModal.jsx
@@ -1,4 +1,6 @@
 var React = require('react');
+var ReactDOM = require('react-dom');
+var ReactDOMServer = require('react-dom/server');
 
 var ErrorModal = React.createClass({
   getDefaultProps: function(){
@@ -6,18 +8,13 @@ var ErrorModal = React.createClass({
       title:'Error'
     });
   },
-
   propTypes:{
     title: React.PropTypes.string,
     message: React.PropTypes.string.isRequired
   },
-  componentDidMount(){
-    var modal = new Foundation.Reveal($('#error-modal'));
-    modal.open();
-  },
-  render(){
+  componentDidMount: function(){
     var {title, message} = this.props;
-    return(
+    var modalMarkUp = (
       <div id="error-modal" className="reveal tiny text-center" data-reveal="">
         <h4>{title}</h4>
         <p>{message}</p>
@@ -28,6 +25,17 @@ var ErrorModal = React.createClass({
         </p>
       </div>
     );
+    var $modal = $(ReactDOMServer.renderToString(modalMarkUp));
+    $(ReactDOM.findDOMNode(this)).html($modal);
+
+    var modal = new Foundation.Reveal($('#error-modal'));
+    modal.open();
+  },
+  render: function(){
+    return(
+      <div>
+      </div>
+    )
   }
 });
 
